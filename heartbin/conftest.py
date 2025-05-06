48782b5b64b31f6aee5eb719cdbc4e1391ba1f0f
Jan Lis <janlis@foxfactory.pl>
2025-05-06 09:36:51 +0200

    project init via cookiecutter django

---
+++ heartbin/conftest.py
@@ -0,0 +1,14 @@
+import pytest
+
+from heartbin.users.models import User
+from heartbin.users.tests.factories import UserFactory
+
+
+@pytest.fixture(autouse=True)
+def _media_storage(settings, tmpdir) -> None:
+    settings.MEDIA_ROOT = tmpdir.strpath
+
+
+@pytest.fixture
+def user(db) -> User:
+    return UserFactory()
